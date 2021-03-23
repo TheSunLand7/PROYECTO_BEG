16419241076bd25cd6d989160af6fb459b5f3c3d
Jinnox <jos.ct101@google.com>
2021-03-23 16:09:45 -0500

    Nueva app de calculadora

--- PUBLIC_PROJECT_INIT/C++/tutorial.cpp
+++ PUBLIC_PROJECT_INIT/C++/tutorial.cpp
@@ -1,8 +1,114 @@
-#include <iostream>
+// include nos dice que vamos a utilizar toda la librería iostream
+#include <iostream>  // iostream: input/output stream
+// Para utilizar el tipo de dato string debemos de incluirlo
+#include <string>
+/* 
+Proporciona un alcance a los identificadores (los nombres de tipos, 
+Funciones, variables, etc.) dentro de él.
+Se utilizan para organizar el código en grupos lógicos y evitar 
+las colisiones de nombres que pueden ocurrir
+*/
 using namespace std;
+
+// En c++ se necesita una funcion principal main; sin ella, no se puede
+// ejecutar el script
+
+/*
+int main()
+{
+	// Si no incluimos la libreria iostream no podremos utilizar el cout,
+	// al igual que si no utilizamos el namespace std.
+	cout << "Hola, soy nuevo aqui! :')\n";  // cout: character output
+	// Si no utilizamos el namespace del tipo std, entonces tendremos que
+	// utilizar el comando "std::" antes del cout y de otros identifiers
+	// lo que es tedioso.
+	// std::cout << "Hola, soy nuevo aqui! :')";
+	cout << "--------------------------" << endl;
+
+	// Una constante no puede cambiar de valor. Si lo haces, te dará error.
+	// Tampoco se puede colocarlo sin valor: "const int gravity;"
+	const int gravity = -9.8;
+
+	// El cin toma el input del usuario
+	// Si colocamos un tipo de dato diferente al asignado, nos imprime "0"
+	// Esto nos indica un error y ademas las siguientes lineas no se ejecutaran
+	
+	int n, x;
+	cout << "Enter a number: ";
+	cin >> n;
+	cout << "Enter next number: ";
+	cin >> x;
+	cout << n << endl;
+	cout << x;
+	
+
+	Si queremos arreglar el error del cin, haremos lo siguiente:
+	int n, x;
+	cout << "Enter a number: ";
+	cin >> n;
+	cout << cin.fail();
+	// cout << cin.fail(); Con este comando se evita que de una respuesta
+	// de error grande y se simplifica a 1 y 0, donde 1 es un error y 0
+	es un correcto
+
+	Otra forma de arreglarlo:
+	int n, x;
+	cout << "Enter a number: ";
+	cin >> n;
+	cin.clear();  // Este metodo solo borra el error, pero no el texto.
+	// Este si elimina el texto; para ello se le da el número de caracteres
+	// a eliminar y ub salto de linea.
+	cin.ignore(1000, '\n');
+	cout << "Enter next number: ";
+	cin >> x;
+	cout << n << endl;
+	cout << x;
+	
+
+	Creando una simple calculadora:
+	int num1, num2;
+	cout << "Enter a number: ";
+	cin >> num1;
+	cin.clear();
+	cin.ignore(1000, '\n');
+	cout << "Enter next number: ";
+	cin >> num2;
+	int sum = num1 + num2;
+	cout << "The sum is: " << sum;
+	
+
+	int x = 8;
+	// Para que el resultado de una operacion sea float, solo uno
+	// de los operandos debe ser float y la tipo de dato de la variable
+	// que sostiene el resultado tambien debe ser float
+	// No se puede sacar el modulo con numeros flotantes.
+	x++;  // El valor se incrementa en 1; x--; disminuye en 1.
+	cout << x;
+	// El return es una regla de c++ que debe incluir al final del codigo
+	// Este retorna 0, porque el main es de tipo integer.
+	return 0;
+}
+*/
 
 int main()
 {
-	cout << "Hola, soy nuevo aqui! :')";
+	
+	// Operadores de comparacion [<, <=, >, >=, ==, !=] 
+	// El resultado es 1 o 0, donde 1 es true y 0 es false
+	bool test = 'a' == 'A';
+	cout << test << endl << endl;
+	cout << "------------------------" << endl;
+
+	int num1, num2;
+	cout << "Enter a number: ";
+	cin >> num1;
+	cin.clear();
+	cin.ignore(1000, '\n');
+	cout << "Enter next number: ";
+	cin >> num2;
+	// Se debe colocar en parentesis la comparacion para que la maquina
+	// sepa que queremos comparar esas dos variables.
+	cout << "These numbers are the same?: " << (num1 == num2);
+
 	return 0;
 }
--- PUBLIC_PROJECT_INIT/PYTHON/APPS/Calc/calcWithGrid.py
+++ PUBLIC_PROJECT_INIT/PYTHON/APPS/Calc/calcWithGrid.py
@@ -2,28 +2,116 @@ from tkinter import *
 
 windows_principal = Tk()
 windows_principal.title("Calculator")
-windows_principal.geometry("305x300")
+windows_principal.geometry("340x360")
 windows_principal.resizable(0, 0)
 windows_principal.iconbitmap("calculadora.ico")
 
 
+# FUNCTIONS
+def numeros(valor):
+	global string
+	string += str(valor)
+	campo.set(string)
+
+
+def operation():
+	global string
+	# state = text_entry.get()
+	result = eval(text_entry.get())
+	campo.set(result)
+	"""
+	if text_entry.get() in ["+", "-", "*", "/", ".", "(", ")"]:
+		pass
+	else:
+		string = ""
+	"""
+	string = ""
+
+
+def delete_all():
+	global string
+	string = ""
+	campo.set(string)
+	string = ""
+
+
+def delete_one():
+	global string
+	current_state = text_entry.get()
+	if len(current_state):
+		new_state = current_state[:-1]   # To save from item 1 to item before last item
+		delete_all()  # Delete the last item left by new_state
+		campo.set(new_state)
+	else:
+		campo.set("Error, there is no input")
+
+
+string = ""
+campo = StringVar()
 # Entrada de texto
-text_entry = Entry(windows_principal, font=("TerminessTTF Nerd Font", 20))
-text_entry.pack(padx=5, fill=X)
+text_entry = Entry(windows_principal, font=("TerminessTTF Nerd Font", 16, "bold"), justify=RIGHT, textvariable=campo)
+text_entry.grid(row=0, column=0, columnspan=4, pady=7, ipadx=52, ipady=5)
 
 # BUTTONS
 # row 1
-Button(windows_principal, text="AC", font=("TerminessTTF Nerd Font", 10, "bold")).place(
-	x=5, y=50, anchor="center", width=40, height=30
-	)
-Button(windows_principal, text="(", font=("TerminessTTF Nerd Font", 10, "bold")).place(
-	x=50, y=50, anchor="center", width=40, height=30
-	)
-Button(windows_principal, text=")", font=("TerminessTTF Nerd Font", 10, "bold")).place(
-	x=95, y=50, anchor="center", width=40, height=30
-	)
-Button(windows_principal, text="/", font=("TerminessTTF Nerd Font", 10, "bold")).place(
-	x=140, y=50, anchor="center", width=40, height=30
-	)
+Button(windows_principal, text="AC", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: delete_all()).grid(
+	row=1, column=0, padx=5, pady=2)
+Button(windows_principal, text="(", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("(")).grid(
+	row=1, column=1, padx=5, pady=2)
+Button(windows_principal, text=")", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros(")")).grid(
+	row=1, column=2, padx=5, pady=2)
+Button(windows_principal, text="/", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("/")).grid(
+	row=1, column=3, padx=5, pady=2)
+
+# BUTTONS
+# row 2
+Button(windows_principal, text="7", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("7")).grid(
+	row=2, column=0, padx=5, pady=2)
+Button(windows_principal, text="8", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("8")).grid(
+	row=2, column=1, padx=5, pady=2)
+Button(windows_principal, text="9", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("9")).grid(
+	row=2, column=2, padx=5, pady=2)
+Button(windows_principal, text="x", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("*")).grid(
+	row=2, column=3, padx=5, pady=2)
+
+# BUTTONS
+# row 3
+
+Button(windows_principal, text="4", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("4")).grid(
+	row=3, column=0, padx=5, pady=2)
+Button(windows_principal, text="5", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("5")).grid(
+	row=3, column=1, padx=5, pady=2)
+Button(windows_principal, text="6", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("6")).grid(
+	row=3, column=2, padx=5, pady=2)
+Button(windows_principal, text="+", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("+")).grid(
+	row=3, column=3, padx=5, pady=2)
+
+# BUTTONS
+# row 4
+Button(windows_principal, text="1", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("1")).grid(
+	row=4, column=0, padx=5, pady=2)
+Button(windows_principal, text="2", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("2")).grid(
+	row=4, column=1, padx=5, pady=2)
+Button(windows_principal, text="3", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("3")).grid(
+	row=4, column=2, padx=5, pady=2)
+Button(windows_principal, text="-", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("-")).grid(
+	row=4, column=3, padx=5, pady=2)
+
+# BUTTONS
+# row 5
+Button(windows_principal, text="0", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros("0")).grid(
+	row=5, column=0, padx=5, pady=2)
+Button(windows_principal, text=".", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: numeros(".")).grid(
+	row=5, column=1, padx=5, pady=2)
+Button(windows_principal, text="<--", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: delete_one()).grid(
+	row=5, column=2, padx=5, pady=2)
+Button(windows_principal, text="=", font=("TerminessTTF Nerd Font", 12, "bold"), width=8, height=2, cursor="hand2", command=lambda: operation()).grid(
+	row=5, column=3, padx=5, pady=2)
+
+# row 6
+Button(
+	windows_principal, text="SALIR", font=("TerminessTTF Nerd Font", 12, "bold"), width=19, height=2, bg="#007fff", fg="#ffffff", cursor="hand2",
+	command=windows_principal.destroy).grid(row=6, column=1, padx=5, pady=5, columnspan=2)
+
 
 windows_principal.mainloop()
